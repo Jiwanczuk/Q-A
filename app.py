fa965941631d3dd52f88838f68d8410510d8d059
Jiwanczuk <103602175+Jiwanczuk@users.noreply.github.com>
2023-02-13 21:55:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import streamlit as st
 from utils import parse_file, embed_text, get_answer
-
 from sidebar import sidebar
 
 sidebar()
 
 st.header("Doc QA")
-uploaded_file = st.file_uploader("Upload a pdf or txt", type=["pdf", "txt"])
+uploaded_file = st.file_uploader("Upload a pdf or txt file", type=["pdf", "txt"])
 
 if uploaded_file is not None:
     index = embed_text(parse_file(uploaded_file))
@@ -14,4 +13,5 @@ if uploaded_file is not None:
     button = st.button("Submit")
     if button:
         st.write(get_answer(index, query))
+
 
