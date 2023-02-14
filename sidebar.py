1fc60bf344e27d4dbec9c785727e478eb36ba4bd
Jiwanczuk <julianiwanczuk@gmail.com>
2023-02-13 21:09:00 -0300

    Initial commit

---
+++ sidebar.py
@@ -0,0 +1,33 @@
+import streamlit as st
+
+
+def sidebar():
+    with st.sidebar:
+        st.markdown("# About")
+        st.markdown(
+            "📖KnowledgeGPT allows you to ask questions about your "
+            "documents and get accurate answers with instant citations. "
+        )
+        st.markdown(
+            "This tool is a work in progress. "
+            "You can contribute to the project on [GitHub](https://github.com/mmz-001/knowledge_gpt) "
+            "with your feedback and suggestions💡"
+        )
+        st.markdown("---")
+        st.markdown(
+            "## How to use\n"
+            "1. Enter your [OpenAI API key](https://platform.openai.com/account/api-keys) below🔑\n"
+            "2. Upload a pdf, docx, or txt file📄\n"
+            "3. Ask a question about the document💬\n"
+        )
+        add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
+
+with st.sidebar:
+    add_radio = st.radio(
+        "Choose a shipping method",
+        ("Standard (5-15 days)", "Express (2-5 days)")
+    )
+        
---
+++ utils.py
@@ -0,0 +1,69 @@
+from flask import Flask, request
+from pypdf import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.llms import OpenAI
+from langchain.vectorstores.faiss import FAISS
+from ratelimiter import RateLimiter
+
+app = Flask(__name__)
+
+def parse_pdf(file):
+    pdf = PdfReader(file)
+    output = []
+    for page in pdf.pages:
+        text = page.extract_text()
+        output.append(text)
+
+    return "\n\n".join(output)
+
+def embed_text(text):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=800, chunk_overlap=0, separators=["\n\n", ".", "?", "!", " ", ""]
+    )
+    texts = text_splitter.split_text(text)
+
+    embeddings = OpenAIEmbeddings()
+    index = FAISS.from_texts(texts, embeddings)
+
+    return index
+
+def get_answer(index, query):
+    docs = index.similarity_search(query)
+
+    chain = load_qa_chain(OpenAI(temperature=0))
+    answer = chain.run(input_documents=docs, question=query)
+
+    return answer
+
+# Set the rate limit to 60 requests per minute
+rate_limiter = RateLimiter(max_calls=60, period=60)
+
+@app.route("/answer", methods=["POST"])
+@rate_limiter
+def answer():
+    text = request.form.get("text")
+    context = request.form.get("context")
+    query = text
+
+    # Use the context to augment the question
+    if context:
+        query = f"{context} {query}"
+
+    index = embed_text(parse_pdf("file.pdf"))
+    answer = get_answer(index, query)
+
+    return {"answer": answer}
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
+
+
+
+
